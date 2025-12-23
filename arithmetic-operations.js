d8a45398dd4393b5d97071e870a2410b61cfa2d1
Анна Теремизова <amura780@gmail.com>
2025-12-24 01:38:05 +0300

    arithmetic operations

---
+++ arithmetic-operations.js
@@ -0,0 +1,65 @@
+const num1 = 50 + 10;
+console.log(num1);
+
+const num2 = 50 - 10;
+console.log(num2);
+
+const num3 = 50 * 10;
+console.log(num3);
+
+const num4 = 50 / 10;
+console.log(num4);
+
+const num5 = 15 % 4;
+console.log(num5);
+
+const n = 2 ** 3;
+console.log(n);
+
+// префиксный инкремент
+let x = 5;
+let z = ++x;
+console.log(x); // 6
+console.log(z); // 6
+
+// постфиксный инкремент
+let a = 5;
+let b = a++;
+console.log(a); // 6
+console.log(b); // 5
+
+// префиксный декремент
+let x2 = 5;
+let z2 = --x2;
+console.log(x2); // 4
+console.log(z2); // 4
+
+// постфиксный декремент
+let a2 = 5;
+let b2 = a2--;
+console.log(a2); // 4
+console.log(b2); // 5
+
+let number1 = 23;
+number1 += 5;
+console.log(number1);
+
+let number2 = 23;
+number2 -= 5;
+console.log(number1);
+
+let number3 = 20;
+number3 *= 2;
+console.log(number3);
+
+let number4 = 5;
+number4 **= 2;
+console.log(number4);
+
+let number5 = 40;
+number5 /= 4;
+console.log(number5);
+
+let number6 = 10;
+number6 %= 3;
+console.log(number6);
